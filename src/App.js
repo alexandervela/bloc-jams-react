68d6a0ac9100bfd2aa1286bbb31391e8e721ad46
Alexander Vela <alexander@Alexanders-MacBook-Pro.local>
2018-05-09 23:23:10 -0400

    checkpoint 10 completed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album'
+import './bloc_jams_logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header>
+      <header className="App-header">
          <nav>
-           <Link to='/'>Landing</Link>
-           <Link to='/library'>Library</Link>
+           <Link className="Nav-bar" to='/'>Home</Link>
+           <Link className="Nav-bar" to='/library'>Library</Link>
          </nav>
-         <h1>Bloc Jams</h1>
+         <h1 className="App-title">
+           <img src={ require("./bloc_jams_logo.png") } alt="Display Bloc Jams Logo"></img>
+         </h1>
        </header>
        <main>
          <Route exact path="/" component={Landing} />
